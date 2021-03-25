1d71543747e27fe38cb2f85be1c69b865748a0e0
SonavAgarwal <42980809+SonavAgarwal@users.noreply.github.com>
2021-03-25 15:42:46 -0700

    github banner, favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import GithubCorner from "react-github-corner";
+import { Helmet } from "react-helmet";
 import Editor from "../components/Editor";
 
 // styles
@@ -11,8 +13,13 @@ const pageStyles = {
 const IndexPage = () => {
     return (
         <main style={pageStyles}>
-            <title>Home Page</title>
+            <Helmet>
+                <meta charSet='utf-8' />
+                <title>Java Swing Component Creator</title>
+                <link rel='shortcut icon' href='../java-swing-component-creator/static/favicon.ico' />
+            </Helmet>
             <Editor />
+            <GithubCorner href='https://github.com/SonavAgarwal/java-swing-component-creator' direction='left' octoColor='#eeeeee' size={"3vw"} />
         </main>
     );
 };
